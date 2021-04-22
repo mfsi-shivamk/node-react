2b1f1bbbc9b3d8dc780ef1481ef1833c23e326e5
mfsi.shivamk <mfsi.shivamk@gmail.com>
2021-04-22 14:05:01 +0530

    hello my first commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <h1>Test
+      <p style={{
+        backgroundColor:"pink",
+        borderStyle: "dotted dashed solid double",
+      border:"2px solid Tomato", backgroundImage: `url(${"static/src/img/main.jpg"})`}}>This is pink some text.</p>
+    <h1>Test
+
+      <p>This is some text.</p>
+      <p>This is some text.</p>
+    </h1>
+    </h1>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+
+    //       <h1> YOLO</h1>
+    //     </p>
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+  );
+}
+
+export default App;
