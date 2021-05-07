f5772274da13195e8d06ec8fda32f3af56e2dc5a
shivamkrishna <74373566+shivamkrishna@users.noreply.github.com>
2021-05-07 21:31:08 +0530

    Login + Register fields test

--- client/src/__tests__/Login.spec.js
+++ client/src/__tests__/Login.spec.js
@@ -7,4 +7,12 @@ describe('<Login />', () => {
 	it('renders without crashing', () => {
 		shallow(<Login />);
 	});
+
+	it('should have the phone and password component', () => {
+		const component = shallow(<Login />);
+		expect(component.find('#phone')).toHaveLength(1);
+		expect(component.find('#password')).toHaveLength(1);
+	});
+
+
 });
--- client/src/__tests__/Register.spec.js
+++ client/src/__tests__/Register.spec.js
@@ -7,4 +7,14 @@ describe('<Register />', () => {
 	it('renders without crashing', () => {
 		shallow(<Register />);
 	});
+
+	it('should have the firstName, lastName, phone, email & password component', () => {
+		const component = shallow(<Register />);
+		expect(component.find('#firstName')).toHaveLength(1);
+		expect(component.find('#lastName')).toHaveLength(1);
+		expect(component.find('#phone')).toHaveLength(1);
+		expect(component.find('#email')).toHaveLength(1);
+		expect(component.find('#password')).toHaveLength(1);
+	});
+
 });
